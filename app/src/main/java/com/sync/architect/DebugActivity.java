a6f2e6b7b2f2a73ba05078e1043b89fb0bd5afd3
sync-ware <saahilms@gmail.com>
2020-03-04 12:57:46 +0000

    Added a debug page to play with individual activities Added setting menus actions
    Signed-off-by: sync-ware <saahilms@gmail.com>

---
+++ app/src/main/java/com/sync/architect/DebugActivity.java
@@ -0,0 +1,48 @@
+package com.sync.architect;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button contactsButton = findViewById(R.id.button_contacts);
+        contactsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DebugActivity.this, ContactsActivity.class));
+            }
+        });
+
+        Button loginButton = findViewById(R.id.button_login);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Start Activity to login page
+            }
+        });
+
+        Button cloudPlans = findViewById(R.id.button_cloud_plans);
+        cloudPlans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Start activity to cloud plans page
+            }
+        });
+
+    }
+
+}
