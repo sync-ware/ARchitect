3a14384539e579a8ed886c17276ee394d9f75b66
Matthew Richardson <matt.rich2000@hotmail.co.uk>
2020-03-11 14:39:47 +0000

    Added JSON file and contacts screen will now display the contacts for the user stored under the relevant email address in JSON file

--- app/src/main/java/com/sync/architect/ContactsActivity.java
+++ app/src/main/java/com/sync/architect/ContactsActivity.java
@@ -1,5 +1,6 @@
 package com.sync.architect;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.content.DialogInterface;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -17,7 +18,16 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.json.JSONException;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class ContactsActivity extends AppCompatActivity {
 
@@ -35,20 +45,12 @@ public class ContactsActivity extends AppCompatActivity {
         Log.d("ContactsActivity", "onCreate: Started.");
         EditText theFilter = (EditText) findViewById(R.id.searchFilter);
         list = findViewById(R.id.listView);
+        names.addAll(readUserContacts("mr864@bath.ac.uk"));
 
         //default list of names for testing
-        names.add("Mitch");
-        names.add("Blake");
-        names.add("Shelly");
-        names.add("Jess");
-        names.add("Steve");
-        names.add("Sharon");
-
 
         adapter = new ArrayAdapter(ContactsActivity.this, android.R.layout.simple_list_item_1 , names);
         list.setAdapter(adapter);
-
-        //
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
@@ -123,6 +125,31 @@ public class ContactsActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private ArrayList<String> readUserContacts(String email) {
+        try {
+            ArrayList<String> contacts = new ArrayList<String>();
+            JSONParser jsonParser = new JSONParser();
+            InputStreamReader reader = new InputStreamReader(getResources().openRawResource(R.raw.contactdata));
+            JSONArray allContacts = (JSONArray) jsonParser.parse(reader);
+
+            for (int i = 0; i < allContacts.size(); i++) {
+                JSONObject currentOBJ = (JSONObject) allContacts.get(i);
+                if (currentOBJ.get("email").equals(email)) {
+                    Collection<String> userContacts = (Collection<String>) currentOBJ.get("contacts");
+                    contacts.addAll(userContacts);
+                    return contacts;
+                }
+            }
+
+            Log.d("JSON contact List", "email does not exists");
+            contacts.add("Mistake");
+            return contacts;
+        } catch (ParseException | IOException ex) {
+            Log.d("Reading JSON error", "JSON is having issues");
+            return null;
+        }
     }
 
     private ArrayList<String> filterList(ArrayList<String> aList, CharSequence charSequence){
